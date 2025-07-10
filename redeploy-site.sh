aa921cf937ba0a4a421d9355895a5dc581c89b34
Mike <nguyentung2107@gmail.com>
2025-07-09 23:22:49 -0700

    updated code

---
+++ redeploy-site.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# kill all server
+tmux kill-server
+
+# go to porfolio directory
+cd MLH-Porfolio-Site
+
+# get the latest change and ignored all uncommited
+git fetch && git reset origin/main --hard
+
+# start new detached tmux session
+tmux new-session -d -s portfolio bash -c "source python3-virtualenv/bin/activate && pip install -r requirements.txt && flask run --host=0.0.0.0"
+
+# confirm
+echo "Redeployed"
+
