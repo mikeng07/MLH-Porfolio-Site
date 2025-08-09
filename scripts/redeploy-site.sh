1a7c48837376c46e905a049a0a3cd424db517585
Mike <nguyentung2107@gmail.com>
2025-08-09 21:59:50 +0000

    update container for VPS

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -6,12 +6,18 @@ cd ~/MLH-Porfolio-Site
 # get the latest change and ignored all uncommited
 git fetch && git reset origin/main --hard
 
+# prevent out of memory issue on VPS
+docker compose -f docker-compose.prod.yml down
+
+# run container
+docker compose -f docker-compose.prod.yml up -d --build 
+
 # start new detached tmux session
-tmux new-session -d -s portfolio bash -c "source python3-virtualenv/bin/activate && pip install -r requirements.txt "
+# tmux new-session -d -s portfolio bash -c "source python3-virtualenv/bin/activate && pip install -r requirements.txt "
 
 # restart myportfolio service
-systemctl restart myportfolio
+# systemctl restart myportfolio
 
 # confirm
-echo "Redeployed"
+# echo "Redeployed"
 
